4134471d4743ed5c43a71ab5beac9734f660615c
rsevial <rebekahsevial@gmail.com>
2023-04-22 00:55:52 +0800

    Create Pseudocode

---
+++ Assign 1.2- Descryption.py
@@ -0,0 +1,17 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Problem 2: Descryption
+
+# Ask user to input string
+# Adding empty list into variable text
+# For loop that will replace the special characters into vowels
+    # If asterisk is inputted, replace it into an a
+    # If ampersand is inputted, replace it into an e
+    # If hashtag is inputted, replace it into an i
+    # If plus sign is inputted, replace it into o
+    # If exclamation point  is inputted, replace it into u
+    # All the text will be append
+# Creating an empty string
+# If the vowel is in the text, assign the string to the vowel
+# Import module
+# Format string
+# Display the string
